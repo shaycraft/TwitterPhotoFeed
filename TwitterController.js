bcb650d3a2e4d82d5c4e351d56d4f1b5c6eed123
Sam <phreakdawg@gmail.com>
2015-04-07 22:09:27 -0600

    adds debug output

--- TwitterController.js
+++ TwitterController.js
@@ -2,15 +2,15 @@
  * Created by Sam on 4/7/2015.
  */
 (function() {
-    $(document).ready(function() {
-       console.log('initializing...');
-    });
 
     var mod = angular.module("TwitterPhotoFeed", []);
     mod.controller("TwitterController", ['$scope', '$http',
         function($scope, $http) {
+            $scope.DebugOutput="";
             $scope.getPhotoFeed = function(txtTwitterId) {
-                alert(txtTwitterId);
+                $scope.DebugOutput = txtTwitterId;
+
+                
             }
 
         }]);
