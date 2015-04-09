211eaae3abe8634ea3753a9b7b28444bf9615b44
Sam <phreakdawg@gmail.com>
2015-04-08 21:48:39 -0600

    sucks

--- TwitterController.js
+++ TwitterController.js
@@ -18,14 +18,22 @@
                 addMessage("txtTwitterId = " + txtTwitterId);
                 $http.get("http://shaycraft.cloudapp.net/TwitterPhotoFeed/gettoken.py")
                     .then(function(response) {
-                       /* var token = response.data;
+                       var token = response.data;
                         addMessage("Got token = " + token);
 
                         var svc_url = 'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=' + txtTwitterId + '&count=200&exclude_replies=true ';
                         var svc_headers = {'Authorization' : 'Bearer ' + token };
                         addMessage('svc_headers = ' + JSON.stringify(svc_headers));
 
-                        var req = {
+                        //serviceModule.$httpProvider.defaults.headers.common['Authorization'] = "Bearer " + token;
+                        $http.defaults.headers.common.Authorization = 'Bearer' + token;
+
+                        $http.get(svc_url)
+                            .then(function(twit_response) {
+                               addMessage("Twitter needs to suck a dick.");
+                            });
+
+                        /*var req = {
                           method: 'GET',
                             url: svc_url,
                             withCredentials: true,
@@ -47,7 +55,7 @@
                                console.log(twit_response);
                             });*/
                         // none of the above shit works, tying something different
-                        var consumerKey = encodeURIComponent('EG256Euv4bVpbcAx6F8QjiCQ3');
+                       /* var consumerKey = encodeURIComponent('EG256Euv4bVpbcAx6F8QjiCQ3');
                         var consumerSecret = encodeURIComponent('p19nAu1GmoyJX2EGeN5DRAk4yJ9jDeFZOiqpsL4gWCbp0JEykv');
                         var credentials = btoa(consumerKey + ':' + consumerSecret);
                         // Twitters OAuth service endpoint
@@ -60,11 +68,11 @@
                             // a successful response will return
                             // the "bearer" token which is registered
                             // to the $httpProvider
-                            addMessage("Got access_token = " + resposne.access_token);
+                            addMessage("Got access_token = " + response.access_token);
                             serviceModule.$httpProvider.defaults.headers.common['Authorization'] = "Bearer " + response.access_token
                         }).error(function (response) {
                             // error handling to some meaningful extent
-                        });
+                        });*/
                     });
             }
         }]);
