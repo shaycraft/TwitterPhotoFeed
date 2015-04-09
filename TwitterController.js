34adefc0a26fc00911026cbaade56f7277744ebe
Sam Haycraft <phreakdawg@gmail.com>
2015-04-08 20:23:47 -0600

    makes call to server for token

--- TwitterController.js
+++ TwitterController.js
@@ -17,18 +17,10 @@
             $scope.getPhotoFeed = function(txtTwitterId) {
                 $scope.DebugOutput = txtTwitterId;
 
-
-                var api_key = 'EG256Euv4bVpbcAx6F8QjiCQ3';
-                var secret_key = 'p19nAu1GmoyJX2EGeN5DRAk4yJ9jDeFZOiqpsL4gWCbp0JEykv';
-                var str_req = api_key + ':' + secret_key;
-                var base64_str = btoa(str_req);
-                var oauthurl = 'https://api.twitter.com/oauth2/token';
-
-                var payload = 'grant_type=client_credentials';
-                var oauthheaders = {'Authorization': 'Basic ' + base64_str,  'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'};
-
-                //console.log(JSON.stringify(oauthheaders));
-                $scope.DebugOutput = addMessage($scope.DebugOutput, JSON.stringify(oauthheaders));
+                $http.get("http://shaycraft.cloudapp.net/TwitterPhotoFeed/gettoken.py")
+                    .then(function(response) {
+                        var token = response.data;
+                    });
             }
 
         }]);
